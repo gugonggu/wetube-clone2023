9b8674a54186fe12dd29b34214fcef2c1620b20c
hongchun <jhc5317@gmail.com>
2023-05-29 22:02:37 +0900

    pug, get, post
    add pug and practicing get, post before real database

---
+++ src/routers/videoRouter.js
@@ -0,0 +1,13 @@
+import express from "express";
+import {
+    watch,
+    getEdit,
+    postEdit,
+    upload,
+    deleteVideo} from "../controllers/videoController"
+
+const videoRouter = express.Router();
+
+videoRouter.get("/:id(\\d+)", watch);
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+export default videoRouter;
--- src/server.js
+++ src/server.js
@@ -1,22 +1,25 @@
 import express from "express";
 import morgan from "morgan";
 
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
+
 const PORT = 4000;
 
 const app = express();
-const loggerMiddleware = morgan("dev");
+const logger = morgan("dev");
 
-const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-};
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 
-const handleHome = (req, res) => {
-    return res.send("I love Middlewares");
-};
+app.use(logger);
 
-app.use(loggerMiddleware);
-app.get("/", logger, handleHome);
+app.use(express.urlencoded({extended: true}));
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () => 
     console.log(`Server listening on port http://localhost:${PORT}`);
