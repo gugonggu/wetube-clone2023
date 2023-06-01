42c981be590502f4a4553f0c81db7a89b3420e81
hongchun <jhc5317@gmail.com>
2023-06-01 23:16:00 +0900

    add session, cookies

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
+import { localsMiddleware } from "./controllers/middlewares";
 
 const app = express();
 const logger = morgan("dev");
@@ -14,6 +16,14 @@ app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 
 app.use(express.urlencoded({extended: true}));
+
+app.use(session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+}));
+
+app.use(localsMiddleware);
 
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
